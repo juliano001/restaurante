5ef7cb97f29d6ea4beac12acd2274b56851b095b
julianofarias <juliano.dsfarias@gmail.com>
2019-10-31 20:23:51 -0300

    added lib

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {runApp(MaterialApp(
+	title: 'Navegação Básica',
+	 theme: ThemeData(
+	   primarySwatch: Colors.blue
+         ),
+         home: PrimeiraRota(),
+      )
+    );
+}
+
+class PrimeiraRota extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira Rota'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Abrir rota'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SegundaRota()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SegundaRota extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Segunda Rota"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () { 
+            Navigator.pop(context);
+          },
+          child: Text('Retornar !'),
+        ),
+      ),
+    );
+  }
+}
